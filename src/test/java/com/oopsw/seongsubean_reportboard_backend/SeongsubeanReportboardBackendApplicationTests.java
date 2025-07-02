342fdba5b5bebf2fbd8e4e0d49c12fc88acb09d1
goldtwo1989 <ttpd19891213@gmail.com>
2025-07-02 14:36:32 +0900

    init: 초기 세팅

---
+++ src/test/java/com/oopsw/seongsubean_reportboard_backend/SeongsubeanReportboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsw.seongsubean_reportboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeongsubeanReportboardBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
